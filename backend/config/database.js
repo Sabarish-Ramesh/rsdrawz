1ac19279d6768bb3c0dd4b5934301f7590f8068b
rs-drawz <sabarishr.21it@kongu.edu>
2024-08-31 19:00:49 +0530

    strict mode enabled

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=>{
+mongoose.set('strictQuery', true);
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(con => {
         console.log(`MongoDB is connected to the host: ${con.connection.host} `)
     })
 }
